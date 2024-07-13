3cb5d079a137dcf308eac0e2b2f44ab853ca1e28
eko.prayugo <eko.prayugo@62teknologi.com>
2024-07-14 01:19:37 +0700

    init

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	app := fiber.New()
+	app.Get("/", sayHello)
+
+	app.Listen(":3000")
+}
+
+func sayHello(c *fiber.Ctx) error {
+	word := "Hello, World!"
+
+	return c.JSON(fiber.Map{
+		"message": word,
+	})
+}
